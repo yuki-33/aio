3d0f7420ca638652fc392ed1ed877ad4da1c3a76
yuki_pisces <pisces.myth.3@gmail.com>
2020-09-25 17:34:18 -0400

    change production/mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,9 +100,9 @@ Rails.application.configure do
     domain: 'gmail.com',
     port:587,
     #gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
-    user_name: Rails.application.credentials.gmail[:mail_address],
+    user_name: ENV['GMAIL_USERNAME'],
     #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
-    password: Rails.application.credentials.gmail[:app_password],
+    password: ENV['GMAIL_PASSWORD'],
     #パスワードをBase64でエンコード
     authentication: :plain
   }
