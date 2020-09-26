c687138429a09db3c02600b54ca6a16c79773a02
yuki_pisces <pisces.myth.3@gmail.com>
2020-09-26 10:23:24 -0400

    301 redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  #herokuapp.comから独自ドメインへリダイレクト
+  before_filter :ensure_domain
+  FQDN = 'www.aiojapanoo.net'
+  #redirect correct server from herokuapp domain for SEO
+  def ensure_domain
+    return unless /\.herokuapp.com/ =~ request.host
+    #主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
+    port = ":#{request.port}" unless [80, 443].include?(request.port)
+    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+  end
 end
