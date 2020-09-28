3bafdbb6bd6bf78d9c5077c8bedb846ef38d0a8f
yuki_pisces <pisces.myth.3@gmail.com>
2020-09-28 17:36:41 -0400

    adding puma

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
-  #herokuapp.comから独自ドメインへリダイレクト
-  before_filter :ensure_domain
-  FQDN = 'aiojapanoo.com'
-  #redirect correct server from herokuapp domain for SEO
-  def ensure_domain
-    return unless /\.herokuapp.com/ =~ request.host
-    #主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
-    port = ":#{request.port}" unless [80, 443].include?(request.port)
-    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
-  end
+  # protect_from_forgery with: :exception
+  # #herokuapp.comから独自ドメインへリダイレクト
+  # before_filter :ensure_domain
+  # FQDN = 'www.aiojapanoo.com'
+  # #redirect correct server from herokuapp domain for SEO
+  # def ensure_domain
+  #   return unless /\.herokuapp.com/ =~ request.host
+  #   #主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
+  #   port = ":#{request.port}" unless [80, 443].include?(request.port)
+  #   redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+  # end
 end
